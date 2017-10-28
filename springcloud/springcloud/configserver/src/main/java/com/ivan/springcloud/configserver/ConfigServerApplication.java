f7a54b486f34c8c88ed24b36d944ad94c60f9748
Liangyi Chen <liangyi.chen@pwc.com>
2017-10-28 13:45:00 +0800

    Setup springcloud parent project and configserver project

---
+++ springcloud/springcloud/configserver/src/main/java/com/ivan/springcloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.ivan.springcloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/** @author lchen283
+ * @Date 2017-10-28 */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
---
+++ springcloud/springcloud/src/main/java/com/ivan/springcloud/springcloud/SpringCloudApplication.java
@@ -0,0 +1,10 @@
+package com.ivan.springcloud.springcloud;
+
+/** @author lchen283
+ * @Date 2017-10-28 */
+public class SpringCloudApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Spring Cloud Application!");
+    }
+}
