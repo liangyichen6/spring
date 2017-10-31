e628cb6751d17ef43f96c976d5aaa8ebb2b7e574
Ivan <cclchen1992@163.com>
2017-10-31 20:52:11 +0800

    refacor

--- springcloud/springcloud/configserver/src/main/java/com/ivan/springcloud/configserver/ConfigServerApplication.java
+++ springcloud/springcloud/configserver/src/main/java/com/ivan/springcloud/configserver/ConfigServerApplication.java
@@ -1,7 +1,7 @@
 package com.ivan.springcloud.configserver;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
@@ -15,6 +15,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		new SpringApplicationBuilder(ConfigServerApplication.class).run(args);
 	}
 }
--- springcloud/springcloud/eurekaserver/readme.md
+++ springcloud/springcloud/eurekaserver/readme.md
@@ -2,9 +2,9 @@
 2. start eureka server jvm configuration
 
 #Instance 1
--Dspring.profiles.active="local" -Dserver.port=8761 -Deureka.instance.hostname="eureka_1" 
+-Dspring.profiles.active="dev" -Dserver.port=8761 -Deureka.instance.hostname="eureka_1" 
 -Deureka.client.serviceUrl.defaultZones="http://localhost:8762/eureka"
 
 #Instance 2
--Dspring.profiles.active="local" -Dserver.port=8762 -Deureka.instance.hostname="eureka_2" 
+-Dspring.profiles.active="dev" -Dserver.port=8762 -Deureka.instance.hostname="eureka_2" 
 -Deureka.client.serviceUrl.defaultZones="http://localhost:8761/eureka"
